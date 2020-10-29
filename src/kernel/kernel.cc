5e7ea33010440cbd0d53eafa8719129fd2f5d838
Nicholas Wong <nicholas_wong@college.harvard.edu>
2020-10-28 23:54:00 -0400

    added types from cs61

--- src/kernel/kernel.cc
+++ src/kernel/kernel.cc
@@ -1,7 +1,9 @@
 #include "kernel/uart.hh"
 #include "kernel/lfb.hh"
 
+#include "common/types.hh"
 #include "common/stdlib.hh"
+
 
 extern "C" void kernel_main() {
     // set up serial console and linear frame buffer
@@ -12,10 +14,11 @@ extern "C" void kernel_main() {
     lfb_print(80, 80, "Hello World!");
 
     // display a UTF-8 string on screen with SSFN
-    lfb_proprint(80, 120, "Please for the love of god boot");
+    lfb_proprint(80, 120, "Bless this little OS!");
 
     // echo everything back
     while(1) {
-        uart_send(uart_getc());
+        uint8_t c = (uint8_t) uart_getc();
+        uart_send((unsigned int) c);
     }
 }
